367789f67c295060eb8753463f7c0db11e1e89a9
nueochon <nueochon@hotmail.com>
2023-02-18 15:40:03 +0700

    setup gh-pages

--- src/report.jsx
+++ src/report.jsx
@@ -8,6 +8,7 @@ import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
 import Table from "react-bootstrap/Table";
 import axios from "axios";
+import Image from 'react-bootstrap/Image'
 import Common from "./common";
 const BASE_URL = Common.API_URL;
 export default class report extends Component {
@@ -64,7 +65,10 @@ export default class report extends Component {
         <Navbar bg="primary" variant="dark">
           <Container>
             <Navbar.Brand href="#">ค้นหาเลขไปรษณีย์</Navbar.Brand>
-            <div style={{color:"#ffffff" , fontSize:"19px"}}>65130841 Nueochon Kaewjittawong</div>
+            <div style={{color:"#ffffff" , fontSize:"19px"}}>
+              <Image src="https://scontent.fbkk15-1.fna.fbcdn.net/v/t1.6435-9/132151530_2115052838627669_1840709369983232305_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHiDzgvVFo7nm6nxYn9EA4kzB882JeeFwDMHzzYl54XAKExve-92lQOAPWSgqVUnPHtTbGZZ5LaEh9wx8DLREE-&_nc_ohc=0eSqdbfjhaAAX8wFoBE&_nc_ht=scontent.fbkk15-1.fna&oh=00_AfBq6hU15Ga-ZiBoQo34La17LBRJkOilXp0echcVIwq8dA&oe=6417E2E1"
+              roundedCircle style={{width:"50px",height:"50px"}}/>
+          {"  "} 65130841 Nueochon Kaewjittawong</div>
           </Container>
         </Navbar>
         <Container>
